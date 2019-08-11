dc6b2ef57bd60a7577e2e32574a77cc687cffb05
Michail <nntndfrk@gmail.com>
2019-08-11 16:57:36 +0300

    added store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {StoreModule} from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../environments/environment';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {SharedModule} from './shared/shared.module';
@@ -27,7 +29,8 @@ import {todoReducer} from './core/redux/todos.reducer';
     // и регистрируем необходимые редьюсеры
     StoreModule.forRoot({
       todosPage: todoReducer
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
